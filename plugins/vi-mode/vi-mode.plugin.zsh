53dc64e0501f2a6718dd3474e2ac98b828fff860
Andrew Janke <andrew@apjanke.net>
2015-07-27 21:21:43 -0400

    vi-mode: remove line-init/finish widgets that broken terminfo keybindings
    The zle-line-init and zle-line-finish definitions here were broken with
    respect to smkx/rmkx because their "if" logic had fallthrough where it shouldn't,
    so the mode was left in rmkx all the time. This just removes those widgets
    entirely, because they're now defined (correctly) in lib/keybindings.zsh and
    not needed in plugins.

--- plugins/vi-mode/vi-mode.plugin.zsh
+++ plugins/vi-mode/vi-mode.plugin.zsh
@@ -1,15 +1,5 @@
-# Ensures that $terminfo values are valid and updates editor information when
-# the keymap changes.
-function zle-keymap-select zle-line-init zle-line-finish {
-  # The terminal must be in application mode when ZLE is active for $terminfo
-  # values to be valid.
-  if (( ${+terminfo[smkx]} )); then
-    printf '%s' ${terminfo[smkx]}
-  fi
-  if (( ${+terminfo[rmkx]} )); then
-    printf '%s' ${terminfo[rmkx]}
-  fi
-
+# Updates editor information when the keymap changes.
+function zle-keymap-select() {
   zle reset-prompt
   zle -R
 }
